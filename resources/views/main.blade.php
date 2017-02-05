2399b4bd20b00269b29f2414a940dd214eda49ac
Alexey Kobyleha <uazonner@gmail.com>
2017-02-06 01:06:25 +0200

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Brainstorm blog</title>
+
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" />
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap-theme.min.css') !!}" />
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/font-awesome.min.css') !!}" />
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/style.css') !!}" />
+
+    <script src="{!! asset('js/jquery.min.js') !!}"></script>
+    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+    <script src="{!! asset('js/script.js') !!}"></script>
+
+</head>
+<body>
+    <header id="main">
+        <div class="container">
+            <div id="blog-info">
+                <div class="blog-name">
+                    <a href="/">Brainstorm</a>
+                </div>
+                <div class="blog-slogan">
+                    <span>create your own web</span>
+                </div>
+            </div>
+            <nav id="main-menu">
+                <ul class="primary">
+                    <li><a href="#">Блог <i class="fa fa-angle-down" aria-hidden="true"></i></a></li>
+                    <li><a class="active" href="#">Новости IT</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <nav id="sub-menu">
+        <div class="container">
+            <div class="row">
+                <ul class="secondary">
+                    <li class="active">
+                        <a href="#">Все материалы</a>
+                    </li>
+                    <li>
+                        <a href="#">HTML & CSS</a>
+                    </li>
+                    <li>
+                        <a href="#">PHP</a>
+                    </li>
+                    <li>
+                        <a href="#">Java Script</a>
+                    </li>
+                    <li>
+                        <a href="#">MOD X</a>
+                    </li>
+                    <li>
+                        <a href="#">DRUPAL</a>
+                    </li>
+                    <li>
+                        <a href="#">LARAVEL</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="breadcrumb">
+
+    </section>
+    <section id="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <article class="post">
+                        <div class="review-content">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <img src="http://fakeimg.pl/207x180/" class="img-post img-responsive" alt="Image">
+                                </div>
+                                <div class="col-lg-8">
+                                    <h4 class="post-title"><a href="#" class="transicion">Тестовый заголовок</a></h4>
+                                    <p>С другой стороны начало повседневной работы по формированию позиции играет важную роль в формировании соответствующий условий активизации.</p>
+                                    <p>Товарищи! дальнейшее развитие различных форм деятельности способствует подготовки и реализации существенных финансовых и административных условий. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="review-info">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <i class="fa fa-clock-o"></i> 20 января 2017 <i class="fa fa-user"></i> Alex-K
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="post">
+                        <div class="review-content">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <img src="http://fakeimg.pl/207x180/" class="img-post img-responsive" alt="Image">
+                                </div>
+                                <div class="col-lg-8">
+                                    <h4 class="post-title"><a href="#" class="transicion">Тестовый заголовок</a></h4>
+                                    <p>С другой стороны начало повседневной работы по формированию позиции играет важную роль в формировании соответствующий условий активизации.</p>
+                                    <p>Товарищи! дальнейшее развитие различных форм деятельности способствует подготовки и реализации существенных финансовых и административных условий. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="review-info">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <i class="fa fa-clock-o"></i> 20 января 2017 <i class="fa fa-user"></i> Alex-K
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="post">
+                        <div class="review-content">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <img src="http://fakeimg.pl/207x180/" class="img-post img-responsive" alt="Image">
+                                </div>
+                                <div class="col-lg-8">
+                                    <h4 class="post-title"><a href="#" class="transicion">Тестовый заголовок</a></h4>
+                                    <p>С другой стороны начало повседневной работы по формированию позиции играет важную роль в формировании соответствующий условий активизации.</p>
+                                    <p>Товарищи! дальнейшее развитие различных форм деятельности способствует подготовки и реализации существенных финансовых и административных условий. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="review-info">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <i class="fa fa-clock-o"></i> 20 января 2017 <i class="fa fa-user"></i> Alex-K
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <aside>
+                        <section class="sidebar">
+                            <div class="input-group">
+                                <input placeholder="Поиск..." class="form-control" type="text">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-ar btn-primary" type="button"><i class="fa fa-search no-margin-right"></i></button>
+                                </span>
+                            </div>
+                        </section>
+                        <section class="sidebar">
+                            <div class="block-wraper">
+                                <div class="block-heading">
+                                    <h4 class="block-title">Категории</h4>
+                                </div>
+                                <div class="block-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atomus, appellat dedocendi omnes quoddam atomos. Vestra. Corrupti sensum multa dissentiet uberius displicet medeam, efficiatur quaeque saluto sollicitare arbitraretur conectitur chaere, deorum consiliisque arbitrer doctrina nasci. Odia malis, scipio, libido. Iudico graviter seditione hoc. Venustate.</p>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="sidebar">
+                            <div class="block-wraper">
+                                <div class="block-heading">
+                                    <h4 class="block-title">Архив</h4>
+                                </div>
+                                <div class="block-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atomus, appellat dedocendi omnes quoddam atomos. Vestra. Corrupti sensum multa dissentiet uberius displicet medeam, efficiatur quaeque saluto sollicitare arbitraretur conectitur chaere, deorum consiliisque arbitrer doctrina nasci. Odia malis, scipio, libido. Iudico graviter seditione hoc. Venustate.</p>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="sidebar">
+                            <div class="block-wraper">
+                                <div class="block-heading">
+                                    <h4 class="block-title">Комментарии</h4>
+                                </div>
+                                <div class="block-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atomus, appellat dedocendi omnes quoddam atomos. Vestra. Corrupti sensum multa dissentiet uberius displicet medeam, efficiatur quaeque saluto sollicitare arbitraretur conectitur chaere, deorum consiliisque arbitrer doctrina nasci. Odia malis, scipio, libido. Iudico graviter seditione hoc. Venustate.</p>
+                                </div>
+                            </div>
+                        </section>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-11 col-md-11">
+                    <p>&copy; Brainstorm, 2016 - 2017.<br>
+                        Копирование и размещение материалов на других сайтах разрешается только с обратной гиперссылкой.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
